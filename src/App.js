df3df87bac623d88a8456db20219a06d0b1138cf
Vadim Fattakhov <vadfthv@gmail.com>
2022-06-01 08:45:47 -0500

    Add Accordion, Semantic UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 
+const items = [
+  {
+    title: "What is React",
+    content: "React is front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
 export default () => {
-  return (
-    <div>
-      <Accordion />
-    </div>
-  );
+  return <Accordion items={items} />;
 };
